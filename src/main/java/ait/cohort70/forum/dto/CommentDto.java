cbfff07b24e5e68041278310d306ff38d6228352
Dushyna <olgavnv777@gmail.com>
2025-12-16 21:24:50 +0100

    @JsonProperty("user")

--- src/main/java/ait/cohort70/forum/dto/CommentDto.java
+++ src/main/java/ait/cohort70/forum/dto/CommentDto.java
@@ -1,18 +1,19 @@
 package ait.cohort70.forum.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class CommentDto {
+    @JsonProperty("user")
     private String userName;
     private String message;
     private LocalDateTime dateCreated;
